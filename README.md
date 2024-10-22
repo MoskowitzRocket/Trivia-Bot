bc87a39e9e9c1b040e7c829c6118aa392494c996
MoskowitzRocket <kalebug@gmail.com>
2024-10-21 19:56:18 -0700

    Got .env working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Trivia-Bot
+## Discord Bot
--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_KEY = os.getenv('APIKEY')
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +17,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(API_KEY)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
